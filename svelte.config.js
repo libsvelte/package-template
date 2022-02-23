b6fea8fc497965dd36ba213466f71162ac54676e
stdlout <99599271+stdlout@users.noreply.github.com>
2022-02-23 19:19:04 +0000

    intial commit

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess({
+        postcss: {
+            plugins: [autoprefixer(), cssnano()]
+        }
+    }),
+
+    kit: {
+        files: {
+            assets: 'static',
+            hooks: 'src/hooks',
+            lib: 'src',
+        },
+        package: {
+            dir: 'dist',
+            emitTypes: true,
+            // excludes all .d.ts and files starting with _ as the name
+            exports: (filepath) => !/^_|\/_|\.d\.ts$/.test(filepath),
+            files: () => true
+          },
+    }
+};
+
+export default config;
